34c7436330cb736337f237460cb1bffe38c80906
mztoja <mztoja@gmail.com>
2023-11-22 21:29:15 +0100

    Start new nest project

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import {NestFactory} from '@nestjs/core';
+import {AppModule} from './app.module';
 
-async function bootstrap() {
+const bootstrap = async (): Promise<void> => {
   const app = await NestFactory.create(AppModule);
   await app.listen(3001);
 }
-bootstrap();
+
+bootstrap().catch(e => console.log('An error occurred while starting the application:', e));
+
+
