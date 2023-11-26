15573fc95615c867bc41afded5b8329ddad6363b
Kamil Lewucha <lewucha2000@gmail.com>
2023-11-26 19:42:15 +0100

    Added missing fields FIRST_NAME and LAST_NAME in HR_PROFILES table

--- src/interfaces/HrProfileInterface.ts
+++ src/interfaces/HrProfileInterface.ts
@@ -1,6 +1,8 @@
 export interface HRProfileInterface {
   id: string;
   userId: string;
+  firstName: string;
+  lastName: string;
   maxReservedStudents: number;
   companyName: string;
   createdAt: string;
