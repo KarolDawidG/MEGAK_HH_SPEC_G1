0b5b1fab0a7f8533a2bf24a5273015e2e043ce00
mztoja <mztoja@gmail.com>
2023-11-26 12:59:39 +0100

    Added TypeOrm imports

--- src/hrProfile/hrProfile.module.ts
+++ src/hrProfile/hrProfile.module.ts
@@ -1,4 +1,8 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { HrProfileEntity } from './hrProfile.entity';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([HrProfileEntity])],
+})
 export class HrProfileModule {}
