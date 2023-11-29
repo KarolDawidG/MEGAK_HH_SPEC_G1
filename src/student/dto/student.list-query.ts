a99637b36b7ba67b4ec05532056f8bc4e08757be
Kamil Lewucha <lewucha2000@gmail.com>
2023-11-29 12:37:28 +0100

    Baisc Query params validation

---
+++ src/student/dto/student.list-query.ts
@@ -0,0 +1,61 @@
+import { Transform } from 'class-transformer';
+import { IsArray, IsBoolean, IsNumber, IsOptional } from 'class-validator';
+import {
+  workTypeEnum,
+  contractTypeEnum,
+} from 'src/interfaces/StudentInterface';
+import { StudentListQueryRequestInterface } from 'src/interfaces/StudentListFilterInterface';
+import { TransformToArray } from 'src/utils/TransformToArray';
+
+export class StudentListQuery implements StudentListQueryRequestInterface {
+  @IsOptional()
+  @Transform(TransformToArray)
+  @IsArray()
+  cc?: number[];
+
+  @IsOptional()
+  @Transform(TransformToArray)
+  @IsArray()
+  ce?: number[];
+
+  @IsOptional()
+  @Transform(TransformToArray)
+  @IsArray()
+  pd?: number[];
+
+  @IsOptional()
+  @Transform(TransformToArray)
+  @IsArray()
+  tpd?: number[];
+
+  @IsOptional()
+  @Transform(TransformToArray)
+  @IsArray()
+  ewt?: workTypeEnum[];
+
+  @IsOptional()
+  @Transform(TransformToArray)
+  @IsArray()
+  ect?: contractTypeEnum[];
+
+  @IsOptional()
+  @Transform(TransformToArray)
+  @IsArray()
+  es?: [number, number];
+
+  @IsOptional()
+  @IsBoolean()
+  cta?: boolean;
+
+  @IsOptional()
+  @IsNumber()
+  moce?: number;
+
+  @IsOptional()
+  @IsNumber()
+  page: number;
+
+  @IsOptional()
+  @IsNumber()
+  pitems: number;
+}
