d3a606ec313c63ac2d2a1fee2d41a204df729deb
mztoja <mztoja@gmail.com>
2023-11-29 19:51:30 +0100

    Added crossOrigin url

--- src/templates/email/adminCreated.ts
+++ src/templates/email/adminCreated.ts
@@ -1,9 +1,11 @@
+import { config } from '../../config/config';
+
 export const adminCreatedEmailTemplate = (
   token: string,
   userid: string,
 ): string => {
   return `
   Zostałeś(aś) dodany(a) do aplikacji MegaK jako administrator.<br/>
-  <a href="https://jakiś tam adres/${userid}/${token}" target="_blank">Kliknij tutaj</a> aby ukończyć rejestrację i aktywować swoje konto.
+  <a href="${config.crossOrigin}/${userid}/${token}" target="_blank">Kliknij tutaj</a> aby ukończyć rejestrację i aktywować swoje konto.
   `;
 };
--- src/templates/email/changePassword.ts
+++ src/templates/email/changePassword.ts
@@ -1,9 +1,11 @@
+import { config } from '../../config/config';
+
 export const changePasswordEmailTemplate = (
   token: string,
   userid: string,
 ): string => {
   return `
   Aby ustawić nowe hasło do naszej aplikacji kliknij w poniższy link:<br/>
-  <a href="https://jakiś tam adres/${userid}/${token}" target="_blank">Kliknij tutaj</a> aby ukończyć rejestrację i aktywować swoje konto.
+  <a href="${config.crossOrigin}/passwordchange/${userid}/${token}" target="_blank">Kliknij tutaj</a> aby ukończyć rejestrację i aktywować swoje konto.
   `;
 };
