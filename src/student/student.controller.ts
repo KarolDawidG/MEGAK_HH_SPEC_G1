8d2e64d42bd0909b8e32be723815d3c94d2c5027
mztoja <mztoja@gmail.com>
2023-12-05 13:10:32 +0100

    adding an access guard

--- src/student/student.controller.ts
+++ src/student/student.controller.ts
@@ -1,13 +1,21 @@
-import { Controller, Get, NotFoundException, Query } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  NotFoundException,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
 import { StudentService } from './student.service';
 import { StudentListQuery } from './dto/student.list-query';
 import { StudentList } from './dto/student.list';
 import { messages } from 'src/config/messages';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('student')
 export class StudentController {
   constructor(private readonly studentService: StudentService) {}
 
+  @UseGuards(AuthGuard('jwt'))
   @Get('/')
   // @UsePipes(
   //   new ValidationPipe({ whitelist: true, forbidNonWhitelisted: false }), // @TODO: Seems to deosn't work
