695c22371fbacfb2e74a3dacaeca6bc18cc6f88b
mztoja <mztoja@gmail.com>
2023-12-06 22:47:24 +0100

    change frontend href of user registration (activation)

--- src/templates/email/studentCreated.ts
+++ src/templates/email/studentCreated.ts
@@ -6,6 +6,6 @@ export const studentCreatedEmailTemplate = (
 ): string => {
   return `
   Zostałeś(aś) dodany(a) do aplikacji MegaK jako kursant.<br/>
-  <a href="${config.crossOrigin}/${userid}/${token}" target="_blank">Kliknij tutaj</a> aby ukończyć rejestrację i aktywować swoje konto.
+  <a href="${config.crossOrigin}/activation/${userid}/${token}" target="_blank">Kliknij tutaj</a> aby ukończyć rejestrację i aktywować swoje konto.
   `;
 };
